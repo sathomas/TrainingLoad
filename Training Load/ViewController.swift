79a7d8de8fe96b18ff02632236203adf5e7fe22c
Stephen Thomas <stephen@sathomas.me>
2019-06-22 12:41:34 -0400

    Initial Commit

---
+++ Training Load/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training Load
+//
+//  Created by Stephen Thomas on 6/22/19.
+//  Copyright © 2019 Stephen Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
